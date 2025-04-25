da768001a649afe3959f12e84f7413fd804eac08
Bandi Kanti Chaitanya <bandikantichaitanya2@gmail.com>
2025-04-25 09:35:59 +0530

    updated version

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 import WeatherDisplay from "./WeatherDisplay";
-// import { event } from "cypress/types/jquery";
+
 
 const App = () => {
   let [weather,setWeather]=useState()
-  function handleWeather(e){
-    // console.log(e.target.value)
-    setWeather(e.target.value)
-  }
+  // function handleWeather(e){
+  //   // console.log(e.target.value)
+  //   setWeather(e.target.value)
+  // }
+  // setWeather(25)
+  useEffect(()=>{
+    setWeather(25)
+  },[])
   return (
     <div>
         {/* Do not remove the main div */}
-        <input type="number" onChange={handleWeather}/>
+        {/* <input type="number" onChange={handleWeather}/> */}
         <WeatherDisplay weather={weather} />
     </div>
   )
